68477e363b71329b25af5a3696b56eb9e0383dbc
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2024-10-20 21:55:02 -0300

    feat: Age calc function

---
+++ src/js/scripts.js
@@ -0,0 +1,96 @@
+const listInputField = document.querySelectorAll(".input-field");
+const listInputData = document.querySelectorAll(".input-field input");
+const buttonElement = document.getElementById("submit-button");
+
+buttonElement.addEventListener("click", () => {
+    let day, monthIndex, year;
+    resetPage();
+    // Error Handling Function 
+    if (errorHandling(listInputData) == 0) {
+        listInputData.forEach(dateInput => {
+            switch (dateInput.id) {
+                case "input-day":
+                    day = dateInput.value;
+                    break;
+                case "input-month":
+                    monthIndex = dateInput.value - 1;
+                    break;
+                case "input-year":
+                    year = dateInput.value;
+            }
+        })
+
+        console.log(day, monthIndex, year);
+
+        let niverData = new Date(year, monthIndex, day);
+
+        document.getElementById("day-value").innerHTML = timeDiffCalc(niverData)[0];
+        document.getElementById("month-value").innerHTML = timeDiffCalc(niverData)[1]; 
+        document.getElementById("year-value").innerHTML = timeDiffCalc(niverData)[2];
+    }
+})
+
+function resetPage() {
+    listInputField.forEach(field => {
+        field.classList.remove("invalid-input");
+    });
+
+    document.querySelectorAll(".result-value").forEach(value => {
+        value.innerHTML = "--";
+    });
+
+}
+
+function timeDiffCalc(niverData) {
+    const dataHoje = new Date();
+    const output = new Array();
+
+    let diffYears = (dataHoje - niverData) / (1000*60*60*24*30*12);
+    let diffMonths = (diffYears - Math.trunc(diffYears)) * 12;
+    let diffDays = (diffMonths - Math.trunc(diffMonths)) * 30;
+
+    // if (dataHoje.getDate() > niverData.getDate() || dataHoje.getMonth() > niverData.getMonth()){
+    //     diffYears--;
+    // }
+
+    if (diffDays != NaN || diffMonths != NaN || diffYears != NaN) {
+        output[0] = Math.trunc(diffDays);
+        output[1] = Math.trunc(diffMonths);
+        output[2] = Math.trunc(diffYears);
+        return output;
+    } else return "--";
+}
+
+function errorHandling(dateEntries) {
+    let output = 0;
+    if (dateEntries[0].value != "") day = dateEntries[0].value;
+    else {
+        console.log("Campo Dia Vazio!");
+        listInputField[0].classList.add("invalid-input");
+        output = -1;
+    }
+
+    if (dateEntries[1].value != "") monthIndex = dateEntries[1].value;
+    else {
+        console.log("Campo Mes Vazio!");
+        listInputField[1].classList.add("invalid-input");
+        output = -1;
+    }
+
+    if (dateEntries[2].value != "") year = dateEntries[2].value;
+    else {
+        console.log("Campo Ano Vazio!");
+        listInputField[2].classList.add("invalid-input");
+        output = -1;
+    }
+    return output;
+}
+
+
+// let inputDay = document.getElementById("input-day");
+// let inputMonth = document.getElementById("input-month");
+// let inputYear = document.getElementById("input-year");
+
+// console.log(inputDay);
+
+// console.log(inputDay.value, "/", inputMonth.value, "/", inputYear.value);
